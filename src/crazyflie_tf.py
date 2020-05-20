9a4d4ab1315dad36221d2374e4c13b261abb8b8d
Charles <charles.sirois@polymtl.ca>
2020-05-20 16:15:34 -0400

    update tf params

--- src/crazyflie_tf.py
+++ src/crazyflie_tf.py
@@ -4,25 +4,27 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped
 
-def handle_crazyflie_pose(msg, cf_name, world):
+def handle_crazyflie_pose(msg, args):
     br = tf.TransformBroadcaster()
-    # pos = ()
+    cf_name = args[0]
+    frame = args[1]
+    world = args[2]
+
     br.sendTransform((msg.pose.position.x, msg.pose.position.y, msg.pose.position.z),
                      (msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w),
                      rospy.Time.now(),
-                     '/crazyflie/%s' % cf_name,
+                     '/%s' % frame,
                      '/%s' % world)
 
 if __name__ == '__main__':
     rospy.init_node('crazyflie_tf_broadcaster')
 
     world = rospy.get_param('~world', 'world')
-    cf_name = rospy.get_param('~cf_name', 'test')
-    
-    
+    cf_name = rospy.get_param('~cf_name')
+    frame = rospy.get_param('~frame')
+
     rospy.Subscriber('/%s/pose' % cf_name,
                      PoseStamped,
                      handle_crazyflie_pose,
-                     cf_name,
-                     world)
+                     (cf_name, frame, world))
     rospy.spin()
