3f82d93f33607a970440e3357e481ec3acebe5d4
Charles <charles.sirois@polymtl.ca>
2020-05-26 15:57:20 -0400

    Emergency service for all cf

--- scripts/swarmManager.py
+++ scripts/swarmManager.py
@@ -14,13 +14,33 @@ from std_srvs import srv
 class Swarm:
     def __init__(self, cf_list):
         self.crazyflies = []
+        self._emergency_list = []
         for each_cf in cf_list:
             self.crazyflies.append(Crazyflie(each_cf))
+            rospy.loginfo("waiting for emergency service of " + each_cf)
+            rospy.wait_for_service('/' + each_cf + '/emergency')
+            rospy.loginfo("found emergency service of " + each_cf)
+            self._emergency_list.append(rospy.ServiceProxy('/' + each_cf + '/emergency', srv.Empty))
+
 
         # Launch services
+        rospy.Service('/update_params', srv.Empty, self.update_params)
+        rospy.Service('/emergency', srv.Empty, self.emergency)
         rospy.Service('/stop', srv.Empty, self.stop)
         rospy.Service('/takeoff', srv.Empty, self.takeOff)   
-        rospy.Service('/land', srv.Empty, self.land)        
+        rospy.Service('/land', srv.Empty, self.land)      
+
+    # Services methods
+    def update_params(self, req):
+        print("UPDATE PARAMS")
+        return srv.EmptyResponse()
+
+    def emergency(self, req):
+        rospy.logerr("EMERGENCY")
+        for each_emergency in self._emergency_list:
+            each_emergency()
+            
+        return srv.EmptyResponse()
 
     def stop(self, req):
         print("STOP")
