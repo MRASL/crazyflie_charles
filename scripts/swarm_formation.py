0c736e900b2c42a68a15e86b67bcd2d090260d2b
Charles <charles.sirois@polymtl.ca>
2020-06-04 13:42:31 -0400

    Line formation

--- scripts/swarm_formation.py
+++ scripts/swarm_formation.py
@@ -52,7 +52,7 @@ class FormationManager:
                            "v": VFormation(self.offset),
                            "pyramid": PyramidFormation(self.offset),
                            "circle": CircleFormation(self.offset),
-                           "line": CircleFormation(self.offset),} 
+                           "line": LineFormation(self.offset),} 
 
         self.start_positions = [] #: (list of list of float) Starting position of the formation, independant of CFs
         
@@ -828,8 +828,6 @@ class CircleFormation(FormationType):
         self.angle_between_cf = 0 #: Angle between each CF (rad)
 
     def check_n(self):
-        # Check if n is a perfect square
-
         if self.n_cf > 0:
             rospy.loginfo("Formation: Formation made of %i crazyflies" % self.n_cf)
         else:
@@ -854,7 +852,7 @@ class CircleFormation(FormationType):
             start_goal = Position()
             start_goal.yaw = 0
             dZ = 0
-            
+
             if i == 0:
                 dX = 0
                 dY = 0
@@ -881,6 +879,83 @@ class CircleFormation(FormationType):
     def update_scale(self):
         self.compute_start_positions()
     
+class LineFormation(FormationType):
+    """Line formation
+
+    Notes:
+        n_cf supported: X
+        scale: Length of the line
+
+    Layouts:
+
+        y
+        |
+        |
+        |_____x
+
+        3
+        
+        2
+
+        1
+
+        0
+        
+    """
+    def __init__(self, offset=[0, 0, 0]):
+        super(LineFormation, self).__init__(offset=offset)
+        
+        self.min_scale = 0.5
+
+        self.cf_dist = 0
+
+    def check_n(self):
+        if self.n_cf > 0:
+            rospy.loginfo("Formation: Formation made of %i crazyflies" % self.n_cf)
+        else:
+            rospy.logerr("Formation: Unsuported number of CFs")
+    
+    # Setter
+    def set_n_cf(self, n):
+        super(LineFormation, self).set_n_cf(n)
+        self.cf_dist = self.scale/(self.n_cf - 1)
+        
+    # Computing
+    def compute_start_positions(self):        
+        cf_num = 0
+        center_x = 0
+        center_y = self.scale / 2
+        center_z = 0
+
+        for i in range(self.n_cf):
+            if rospy.is_shutdown(): break
+
+            start_goal = Position()
+            start_goal.yaw = 0
+            dZ = 0
+            
+            dX = 0
+            dY = self.cf_dist*i
+            
+            start_goal.x = center_x + dX             
+            start_goal.y = dY
+            start_goal.z = center_z + dZ
+
+            self.cf_goals[cf_num] = start_goal
+
+            center_dist, theta, center_height = self.compute_info_from_center([start_goal.x, start_goal.y, start_goal.z], [center_x, center_y, 0])
+            self.center_dist[cf_num] = center_dist
+            self.angle[cf_num] = theta
+            self.center_height[cf_num] = center_height
+
+            cf_num += 1
+        
+        return self.cf_goals
+    
+    def update_scale(self):
+        self.cf_dist = self.scale / (self.n_cf - 1)
+        self.compute_start_positions()
+
 class SoloFormation(FormationType):
     def __init__(self, offset=[0, 0, 0]):
         n_cf_supported = [1]
