f5da8560c8056761fd38856442c1dc2eade3e46c
Charles <charles.sirois@polymtl.ca>
2020-07-16 15:25:32 -0400

    Better hovering stabilization

--- swarm_manager/demos/hover.py
+++ swarm_manager/demos/hover.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+"""
+Script to test hovering a single CF
+"""
+
+from threading import Thread
 import rospy
-from crazyflie_driver.msg import Hover, Position
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Empty
 from crazyflie_driver.srv import UpdateParams
-from threading import Thread
+from crazyflie_driver.msg import Hover, Position
 
 GND_HEIGHT = 0.2
 
-class Crazyflie:
+class Crazyflie(object):
     def __init__(self, prefix):
         self.prefix = prefix
 
@@ -21,10 +25,10 @@ class Crazyflie:
         rospy.loginfo("found update_params service")
 
         self.update_params = rospy.ServiceProxy(prefix + '/update_params', UpdateParams)
-        self.setParam("commander/enHighLevel", 1)
-        self.setParam("stabilizer/estimator", 2) # Use EKF
-        self.setParam("stabilizer/controller", 1) # 1: High lvl, 2: Mellinger
-        self.setParam("kalman/resetEstimation", 1)
+        self.set_param("commander/enHighLevel", 1)
+        self.set_param("stabilizer/estimator", 2) # Use EKF
+        self.set_param("stabilizer/controller", 1) # 1: High lvl, 2: Mellinger
+        self.set_param("kalman/resetEstimation", 1)
 
         self.hover_pub = rospy.Publisher(prefix + "/cmd_hover", Hover, queue_size=1)
         self.hover_msg = Hover()
@@ -46,41 +50,15 @@ class Crazyflie:
         rospy.Subscriber("/%s/pose" % prefix, PoseStamped, self.pose_handler)
         self.cf_pose = PoseStamped()
 
-    def setParam(self, name, value):
+    def set_param(self, name, value):
         rospy.set_param(self.prefix + "/" + name, value)
         self.update_params([name])
 
     def pose_handler(self, req):
         self.cf_pose = req
 
-    # x, y is the x, y distance relative to itself
-    # z is absolute z distance
-    def goTo (self, x, y, zDistance, yaw):
-        pass
-
-    def hover(self, duration):
-        start_position = self.cf_pose
-        start_x = start_position.pose.position.x
-        start_y = start_position.pose.position.y
-        start_z = start_position.pose.position.z
-
-        start = rospy.get_time()
-        while not rospy.is_shutdown():
-            self.pose_msg.x = start_x
-            self.pose_msg.y = start_y
-            self.pose_msg.z = start_z
-
-            now = rospy.get_time()
-            if now - start > duration:
-                break
-
-            self.pose_msg.header.seq += 1
-            self.pose_msg.header.stamp = rospy.Time.now()
-            self.pose_pub.publish(self.pose_msg)
-            self.rate.sleep()
-
     # take off to z distance
-    def takeOff(self, zDistance):
+    def take_off(self, zDistance):
         print "TAKE OFF"
         time_range = 1 + int(10*zDistance/0.4)
         while not rospy.is_shutdown():
@@ -122,13 +100,57 @@ class Crazyflie:
                 zDistance -= 0.05
         self.stop_pub.publish(self.stop_msg)
 
+    def hover_pos(self, duration):
+        start_position = self.cf_pose
+        start_x = start_position.pose.position.x
+        start_y = start_position.pose.position.y
+        start_z = start_position.pose.position.z
+
+        start = rospy.get_time()
+        while not rospy.is_shutdown():
+            self.pose_msg.x = start_x
+            self.pose_msg.y = start_y
+            self.pose_msg.z = start_z
+
+            now = rospy.get_time()
+            if now - start > duration:
+                break
+
+            self.pose_msg.header.seq += 1
+            self.pose_msg.header.stamp = rospy.Time.now()
+            self.pose_pub.publish(self.pose_msg)
+            self.rate.sleep()
+
+    def hover_hov(self, duration):
+        start_position = self.cf_pose
+        # start_x = start_position.pose.position.x
+        # start_y = start_position.pose.position.y
+        start_z = start_position.pose.position.z
+
+        start = rospy.get_time()
+        while not rospy.is_shutdown():
+            self.hover_msg.vx = 0.0
+            self.hover_msg.vy = 0.0
+            self.hover_msg.yawrate = 0.0
+            self.hover_msg.zDistance = start_z
+
+            now = rospy.get_time()
+            if now - start > duration:
+                break
+
+            self.hover_msg.header.seq += 1
+            self.hover_msg.header.stamp = rospy.Time.now()
+            self.hover_pub.publish(self.hover_msg)
+            self.rate.sleep()
+
 def handler(cf):
     print "TAKE OFF IN 3 SEC"
     rospy.sleep(3)
-    cf.takeOff(1.0)
+    cf.take_off(1.0)
 
     print "HOVERING"
-    cf.hover(10.0)
+    # cf.hover_hov(10.0)
+    cf.hover_pos(10.0)
 
     print "LANDING"
     cf.land()
