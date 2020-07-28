5ac6ba3f02dd15f506f5a3e5bfd682d77e1fd0da
Charles <charles.sirois@polymtl.ca>
2020-07-28 11:33:54 -0400

    documentation path

--- build.sh
+++ build.sh
@@ -3,46 +3,21 @@ set -e
 
 ROOT=$PWD
 
-# submodules
+echo "Starting package installation"
+
+# Install crazyflie ros
+echo "Installing crazyflie ros"
+cd ros_ws/src/
+git clone https://github.com/whoenig/crazyflie_ros.git
+cd crazyflie_ros
 git submodule init
 git submodule update
-
-cd ros_ws/src/crazyflie_ros/
-git submodule init
-git submodule update
-cd $ROOT
-
-cd ros_ws/src/externalDependencies/libmotioncapture/
-git submodule init
-git submodule update
-cd $ROOT
-
-# build nrf firmware
-cd crazyflie2-nrf-firmware
-#tools/build/download_deps
-make BLE=0
-cd $ROOT
-
-# build stm firmware
-cd crazyflie-firmware
-git submodule init
-git submodule update
-make
-cd $ROOT
-
-# build radio firmware
-cd crazyradio-firmware/firmware
-make CRPA=1
-cd $ROOT
-
-# build simulator firmware backend
-cd ros_ws/src/crazyswarm/scripts/pycrazyswarm/cfsim
-make
 cd $ROOT
 
 # ros
+echo "ROS workspace initialization"
 cd ros_ws
-# -k: hack for dependency issues
-catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -k
 catkin_make
 cd $ROOT
+
+echo "Installation done"
---
+++ demos/formation_demo.py
@@ -0,0 +1,55 @@
+"""Demo script to move swarm in formation.
+
+Press 'O' in case of emergency.
+
+Move formation with joystick sticks
+"""
+
+import os
+import rospy
+
+# pylint: disable=invalid-name
+# pylint: disable=wrong-import-position
+# pylint: disable=import-error
+
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+api_path = os.path.join(parentdir, 'scripts')
+os.sys.path.insert(0, api_path)
+
+from swarm_api import SwarmAPI
+
+def init_joystick():
+    """Start joystick and link buttons
+    """
+    swarm.start_joystick()
+
+    swarm.link_joy_button("S", swarm.take_off)
+    swarm.link_joy_button("X", swarm.land)
+    swarm.link_joy_button("O", swarm.emergency)
+    swarm.link_joy_button("T", swarm.toggle_ctrl_mode)
+
+    swarm.link_joy_button("DL", swarm.prev_formation)
+    swarm.link_joy_button("DR", swarm.next_formation)
+    swarm.link_joy_button("DU", swarm.inc_scale)
+    swarm.link_joy_button("DD", swarm.dec_scale)
+
+    swarm.link_joy_button("L2", swarm.set_formation, "v")
+
+if __name__ == "__main__":
+    swarm = SwarmAPI()
+
+    init_joystick()
+
+    print "Formation demo"
+    swarm.set_mode("formation")
+    swarm.set_formation("v")
+
+    swarm.take_off()
+    rospy.sleep(10)
+
+    print "Moving formation to (2, 2, 0.5)"
+    swarm.set_formation("pyramid")
+
+    # swarm.go_to({'formation': [2, 2, 0.5, 0]})
+
+    rospy.spin()
