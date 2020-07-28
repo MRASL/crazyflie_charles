cb00680e7a9b76b1c25ce66f67f6a40e08ec803b
Charles <charles.sirois@polymtl.ca>
2020-07-28 11:01:47 -0400

    Files architecture

---
+++ build.sh
@@ -0,0 +1,48 @@
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+ROOT=$PWD
+
+# submodules
+git submodule init
+git submodule update
+
+cd ros_ws/src/crazyflie_ros/
+git submodule init
+git submodule update
+cd $ROOT
+
+cd ros_ws/src/externalDependencies/libmotioncapture/
+git submodule init
+git submodule update
+cd $ROOT
+
+# build nrf firmware
+cd crazyflie2-nrf-firmware
+#tools/build/download_deps
+make BLE=0
+cd $ROOT
+
+# build stm firmware
+cd crazyflie-firmware
+git submodule init
+git submodule update
+make
+cd $ROOT
+
+# build radio firmware
+cd crazyradio-firmware/firmware
+make CRPA=1
+cd $ROOT
+
+# build simulator firmware backend
+cd ros_ws/src/crazyswarm/scripts/pycrazyswarm/cfsim
+make
+cd $ROOT
+
+# ros
+cd ros_ws
+# -k: hack for dependency issues
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -k
+catkin_make
+cd $ROOT
